88bc84ff7995018a833effa998d4212160b7930f
simonlennon <simon@simonlennon.com>
2014-05-29 18:44:35 +0100

    Adding a schedule

--- src/main/java/com/simonlennon/automate/PersistedProperties.java
+++ src/main/java/com/simonlennon/automate/PersistedProperties.java
@@ -1,5 +1,8 @@
 package com.simonlennon.automate;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -11,31 +14,63 @@ import java.util.Properties;
  */
 public class PersistedProperties {
 
-    private static PersistedProperties pp = null;
+    private static Logger logger = LogManager
+            .getLogger(PersistedProperties.class);
 
-    private static final String FILE_NAME = "scheduler.properties";
+    private static String PROP_DIR_KEY = "automate.props.dir";
+
+    private static PersistedProperties pp;
+
+    private static final String FILE_NAME = "automate.properties";
 
     private Properties props = new Properties();
 
     private PersistedProperties() {
     }
 
-    public synchronized static PersistedProperties getInstance() throws IOException {
+    public synchronized static PersistedProperties getInstance() {
+
+        try {
+            return getInstanceInternal();
+        } catch (IOException e) {
+           throw new RuntimeException("Error loading properties",e);
+        }
+
+    }
+
+    private synchronized static PersistedProperties getInstanceInternal() throws IOException {
+
         if (pp == null) {
             pp = new PersistedProperties();
-            File f = new File(FILE_NAME);
+
+            File f;
+            String propDir = System.getProperty(PROP_DIR_KEY);
+            if (propDir != null && propDir.trim().length() != 0) {
+                f = new File(propDir, FILE_NAME);
+            } else {
+                f = new File(FILE_NAME);
+            }
+
+            logger.debug("Loading props from: "+f.getPath());
+
             if (!f.exists()) {
                 f.createNewFile();
             }
+
             pp.props.load(new FileInputStream(f));
         }
+
         return pp;
     }
 
-    public synchronized void saveProp(String key, String value) throws IOException {
+    public synchronized void saveProp(String key, String value) {
         File f = new File(FILE_NAME);
         props.put(key, value);
-        props.store(new FileOutputStream(f), "Last write: key="+key+" value="+value);
+        try {
+            props.store(new FileOutputStream(f), "Last write: key=" + key + " value=" + value);
+        } catch (IOException e) {
+            throw new RuntimeException("Error saving prop",e);
+        }
     }
 
     public String getProp(String key) {
@@ -44,14 +79,6 @@ public class PersistedProperties {
             return value.toString();
         } else {
             return null;
-        }
-    }
-
-    public static void main(String args[]){
-        try {
-            getInstance();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
