8fe1be223105f75ecdcbc3266fe66783f932dcb9
simonlennon <simon@simonlennon.com>
2014-08-26 08:42:35 +0100

    Adding a schedule

--- src/main/java/com/simonlennon/automate/heating/Boiler.java
+++ src/main/java/com/simonlennon/automate/heating/Boiler.java
@@ -1,9 +1,13 @@
 package com.simonlennon.automate.heating;
 
+import com.pi4j.io.gpio.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Date;
+
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 /**
  * Created by simon.lennon on 20/01/14.
@@ -14,26 +18,33 @@ public class Boiler {
     protected Date lastOn;
     protected Date lastOff;
 
+    protected GpioPinDigitalOutput pin;
+    protected GpioController gpio;
+
+
     private static Logger  logger = LogManager.getLogger(Boiler.class);
 
     public Boiler() {
+
+        gpio = GpioFactory.getInstance();
+        pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "BOILER_RELAY", PinState.LOW);
 
     }
 
     public void turnOn() {
         logger.info("Boiler turning on");
+        pin.high();
         lastOn = new Date();
-        on = true;
     }
 
     public void turnOff() {
         logger.info("Boiler turning off");
+        pin.low();
         lastOff = new Date();
-        on = false;
     }
 
     public boolean isOn() {
-        return on;
+        return pin.isHigh();
     }
 
 }
--- src/main/java/com/simonlennon/automate/heating/Rads.java
+++ src/main/java/com/simonlennon/automate/heating/Rads.java
@@ -1,5 +1,6 @@
 package com.simonlennon.automate.heating;
 
+import com.pi4j.io.gpio.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,22 +15,27 @@ public class Rads {
     protected Date lastOn;
     protected Date lastOff;
 
+    protected GpioPinDigitalOutput pin;
+    protected GpioController gpio;
+
+
     private static Logger logger = LogManager.getLogger(Boiler.class);
 
     public Rads(){
-
+        gpio = GpioFactory.getInstance();
+        pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "RADS_RELAY", PinState.LOW);
     }
 
     public void turnOn() {
         logger.info("Rads turning on");
+        pin.high();
         lastOn = new Date();
-        on = true;
     }
 
     public void turnOff() {
         logger.info("Rads turning off");
+        pin.low();
         lastOff = new Date();
-        on = false;
     }
 
     public boolean isOn() {
