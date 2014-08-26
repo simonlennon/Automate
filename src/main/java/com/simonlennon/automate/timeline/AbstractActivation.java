2946f330f73a49eda082d18bc1c642deed4d2fe2
simonlennon <simon@simonlennon.com>
2014-08-26 08:48:01 +0100

    Adding a schedule

--- src/main/java/com/simonlennon/automate/timeline/AbstractActivation.java
+++ src/main/java/com/simonlennon/automate/timeline/AbstractActivation.java
@@ -1,8 +1,8 @@
 package com.simonlennon.automate.timeline;
 
-import com.simonlennon.automate.timeline.Activation;
+        import com.simonlennon.automate.timeline.Activation;
 
-import java.util.Date;
+        import java.util.Date;
 
 /**
  * Created by simon.lennon on 23/01/14.
@@ -24,20 +24,4 @@ public abstract class AbstractActivation implements Activation {
         return endTime;
     }
 
-    public boolean getFireRads() {
-        return false;
-    }
-
-    /**
-     * The tank is always fired when the boiler is running. There is no tank valve on our system.
-     *
-     * @return
-     */
-    public boolean getFireTank() {
-        return true;
-    }
-
-    public int getMaxTankLevel() {
-        return 0;
-    }
 }
